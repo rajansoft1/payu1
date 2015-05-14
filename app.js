9fea582cf2b9fe0680e87cc9e5efb7482e634f3a
rajan chaudhary <rajanchaudhary@TDV-GRAPX-OS-X-YOSEMITE.local>
2015-05-14 12:15:23 +0530

    added comment and formatting

--- app.js
+++ app.js
@@ -1,108 +1,118 @@
-(function(){var data = [
-{
+	(function(){
+	//data, i have copiedit directly instead of making an ajax request, we can do it for ajax request too, but its design will be
+	//different from existing design
+		var data = [
+	{
 
-"paymentId" : 435456,
-"orderDate" : "05/11/2015",
-"merchatId" : 3245,
-"customerEmail" : "varun@gmail.com",
-"amount" : 234.56,
-"paymentStatus" : 'Initiated'
+	"paymentId" : 435456,
+	"orderDate" : "05/11/2015",
+	"merchatId" : 3245,
+	"customerEmail" : "varun@gmail.com",
+	"amount" : 234.56,
+	"paymentStatus" : 'Initiated'
 
-},
-{
+	},
+	{
 
-"paymentId" : 435466,
-"orderDate" : "06/21/2015",
-"merchatId" : 3289,
-"customerEmail" : "rahul@gmail.com",
-"amount" : 122.66,
-"paymentStatus" : 'Failed'
+	"paymentId" : 435466,
+	"orderDate" : "06/21/2015",
+	"merchatId" : 3289,
+	"customerEmail" : "rahul@gmail.com",
+	"amount" : 122.66,
+	"paymentStatus" : 'Failed'
 
-},
-{
+	},
+	{
 
-"paymentId" : 435459,
-"orderDate" : "02/23/2015",
-"merchatId" : 4567,
-"customerEmail" : "alok@gmail.com",
-"amount" : 4567.33,
-"paymentStatus" : 'Initiated'
+	"paymentId" : 435459,
+	"orderDate" : "02/23/2015",
+	"merchatId" : 4567,
+	"customerEmail" : "alok@gmail.com",
+	"amount" : 4567.33,
+	"paymentStatus" : 'Initiated'
 
-},
-{
+	},
+	{
 
-"paymentId" : 435476,
-"orderDate" : "04/06/1989",
-"merchatId" : 3245,
-"customerEmail" : "rohit@gmail.com",
-"amount" : 23345,
-"paymentStatus" : 'Success'
+	"paymentId" : 435476,
+	"orderDate" : "04/06/1989",
+	"merchatId" : 3245,
+	"customerEmail" : "rohit@gmail.com",
+	"amount" : 23345,
+	"paymentStatus" : 'Success'
 
-},
-{
+	},
+	{
 
-"paymentId" : 112345,
-"orderDate" : "08/24/2015",
-"merchatId" : 3245,
-"customerEmail" : "sapna@gmail.com",
-"amount" : 567.96,
-"paymentStatus" : 'Refunded'
+	"paymentId" : 112345,
+	"orderDate" : "08/24/2015",
+	"merchatId" : 3245,
+	"customerEmail" : "sapna@gmail.com",
+	"amount" : 567.96,
+	"paymentStatus" : 'Refunded'
 
-},
-{
+	},
+	{
 
-"paymentId" : 345654,
-"orderDate" : "05/11/2015",
-"merchatId" : 3245,
-"customerEmail" : "kamal@gmail.com",
-"amount" : 2434.43,
-"paymentStatus" : 'Initiated'
+	"paymentId" : 345654,
+	"orderDate" : "05/11/2015",
+	"merchatId" : 3245,
+	"customerEmail" : "kamal@gmail.com",
+	"amount" : 2434.43,
+	"paymentStatus" : 'Initiated'
 
-},
-{
+	},
+	{
 
-"paymentId" : 435451,
-"orderDate" : "03/10/2015",
-"merchatId" : 3245,
-"customerEmail" : "shrey@gmail.com",
-"amount" : 729.56,
-"paymentStatus" : 'Dropped'
+	"paymentId" : 435451,
+	"orderDate" : "03/10/2015",
+	"merchatId" : 3245,
+	"customerEmail" : "shrey@gmail.com",
+	"amount" : 729.56,
+	"paymentStatus" : 'Dropped'
 
-},
-{
+	},
+	{
 
-"paymentId" : 435486,
-"orderDate" : "03/16/2015",
-"merchatId" : 3245,
-"customerEmail" : "amar@gmail.com",
-"amount" : 3947.57,
-"paymentStatus" : 'Initiated'
+	"paymentId" : 435486,
+	"orderDate" : "03/16/2015",
+	"merchatId" : 3245,
+	"customerEmail" : "amar@gmail.com",
+	"amount" : 3947.57,
+	"paymentStatus" : 'Initiated'
 
-}
-]
+	}
+	]
 
-var element = document.getElementById('myGrid');
+	//element on which we have to bind grid
+	var element = document.getElementById('myGrid');
 
-var statusFormatter = function(elem){
-if(elem.innerHTML == 'Initiated'){
-		elem.className += ' initiated';
-}
-if(elem.innerHTML == 'Failed'){
-		elem.className += ' failed';
-}
-if(elem.innerHTML == 'Dropped'){
-		elem.className += ' dropped';
-}
-if(elem.innerHTML == 'Success'){
-		elem.className += ' success';
-}
-if(elem.innerHTML == 'Refunded'){
-		elem.className += ' refunded';
-}
-return elem;
-}
-grid(data, element, {formatting: {'paymentStatus': statusFormatter}, pageSize: 4, sorting: ['paymentId','orderDate','amount' ], filters: ['paymentStatus']}).Init();
-})();
+	//formatter for status
+	var statusFormatter = function(elem){
+	if(elem.innerHTML == 'Initiated'){
+			elem.className += ' initiated';
+	}
+	if(elem.innerHTML == 'Failed'){
+			elem.className += ' failed';
+	}
+	if(elem.innerHTML == 'Dropped'){
+			elem.className += ' dropped';
+	}
+	if(elem.innerHTML == 'Success'){
+			elem.className += ' success';
+	}
+	if(elem.innerHTML == 'Refunded'){
+			elem.className += ' refunded';
+	}
+	return elem;
+	}
+	//initiate grid
+	grid(data, element, {formatting: {'paymentStatus': statusFormatter},
+	 pageSize: 4,
+	  sorting: ['paymentId','orderDate','amount' ],
+	   filters: ['paymentStatus']
+	}).Init();
+	})();
 
 
 
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var util = {
+  //this function will extend the source object with target one
           extend: function(a, b){
             for(var key in b)
               if(b.hasOwnProperty(key))
               a[key] = b[key];
               return a;
           },
+          //check if string is date
           isDate: function(val) {
            var date = new Date(val);
             return !isNaN(date.valueOf());
           },
+          //it will serialize the object in querystring format
           serializeObj: function (obj) {
               var str = [];
               for (var p in obj)
@@ -17,7 +20,7 @@ var util = {
                   }
               return '?' + str.join("&");
           },
-
+          //it will remove class from string and return string
           removeClass: function (classes, classToRemove) {
               var index = classes.indexOf(classToRemove)
               if (index > -1)
@@ -25,6 +28,7 @@ var util = {
 
               return classes
           },
+          //it will add a new class 
           addClass: function (classes, classToAdd) {
               var index = classes.indexOf(classToAdd)
               if (index == -1) {
